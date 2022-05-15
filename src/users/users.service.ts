c639eabca0e5fe51f1aaab30db7faaea9e26f7a2
Mangpha <8350130@gmail.com>
2022-05-15 19:26:06 +0900

    JWT Module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,12 +6,14 @@ import { LoginInput } from './dtos/login-dto';
 import { User } from './entities/user.entity';
 import * as jwt from 'jsonwebtoken';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from 'src/jwt/jwt.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
     private readonly config: ConfigService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async createAccount({
@@ -46,8 +48,8 @@ export class UsersService {
       if (!user) return { ok: false, error: 'User Not Found' };
       const checkPassword = await user.checkPassword(password);
       if (!checkPassword) return { ok: false, error: 'Wrong Password' };
-      const token = jwt.sign({ id: user.id }, this.config.get('SECRET_KEY'));
-      return { ok: true, token: 'token' };
+      const token = this.jwtService.sign(user.id);
+      return { ok: true, token };
     } catch (e) {
       return {
         ok: false,
