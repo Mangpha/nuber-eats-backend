8e6cd3a8dc8200a3e1207734048e78572a26a15d
Mangpha <8350130@gmail.com>
2022-06-27 17:42:51 +0900

    Create Restaurants Service, Resolver

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,9 +1,16 @@
-import { ArgsType, OmitType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
+import { CoreOutput } from 'src/common/dtos/output.dto';
 import { Restaurant } from '../entities/restaurant.entity';
 
-@ArgsType()
-export class CreateRestaurantDto extends OmitType(
-  Restaurant,
-  ['id'],
-  ArgsType,
-) {}
+@InputType()
+export class CreateRestaurantInput extends PickType(Restaurant, [
+  'name',
+  'coverImg',
+  'address',
+]) {
+  @Field((type) => String)
+  categoryName: string;
+}
+
+@ObjectType()
+export class CreateRestaurantOutput extends CoreOutput {}
--- src/restaurants/dtos/update-restaurant.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Args, ArgsType, Field, InputType, PartialType } from '@nestjs/graphql';
-import { Restaurant } from '../entities/restaurant.entity';
-import { CreateRestaurantDto } from './create-restaurant.dto';
-
-@ArgsType()
-export class UpdateRestaurantDto extends PartialType(Restaurant, ArgsType) {
-  @Field((type) => Number)
-  id: number;
-}
