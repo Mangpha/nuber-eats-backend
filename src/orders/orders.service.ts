6016715c7ddffc62906c71f24f3a30eaa7840390
Mangpha <8350130@gmail.com>
2022-07-12 17:56:30 +0900

    editOrder Resolver Service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -5,10 +5,11 @@ import { RestaurantRepository } from 'src/restaurants/repositories/restaurant.re
 import { User, UserRole } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { EditOrderInput, EditOrderOutput } from './dtos/edit-order.dto';
 import { GetOrderInput, GetOrderOutput } from './dtos/get-order.dto';
 import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
 import { OrderItem } from './entities/order-item.entity';
-import { Order } from './entities/order.entity';
+import { Order, OrderStatus } from './entities/order.entity';
 
 @Injectable()
 export class OrdersService {
@@ -19,6 +20,17 @@ export class OrdersService {
     @InjectRepository(Dish) private readonly dishes: Repository<Dish>,
     private readonly restaurants: RestaurantRepository,
   ) {}
+
+  canSeeOrder(user: User, order: Order): boolean {
+    let allowed = true;
+    if (user.role === UserRole.Client && order.customerId !== user.id)
+      allowed = false;
+    if (user.role === UserRole.Delivery && order.driverId !== user.id)
+      allowed = false;
+    if (user.role === UserRole.Owner && order.restaurant.ownerId !== user.id)
+      allowed = false;
+    return allowed;
+  }
 
   async createOrder(
     customer: User,
@@ -119,18 +131,48 @@ export class OrdersService {
       });
       if (!order) return { ok: false, error: 'Order not found' };
 
-      let allowed = true;
-      if (user.role === UserRole.Client && order.customerId !== user.id)
-        allowed = false;
-      if (user.role === UserRole.Delivery && order.driverId !== user.id)
-        allowed = false;
-      if (user.role === UserRole.Owner && order.restaurant.ownerId !== user.id)
-        allowed = false;
-      if (!allowed) return { ok: allowed, error: 'You cannot see that' };
+      if (!this.canSeeOrder(user, order))
+        return { ok: false, error: 'You cannot see that' };
 
       return { ok: true, order };
     } catch {
       return { ok: false, error: 'Could not get order' };
+    }
+  }
+
+  async editOrder(
+    user: User,
+    { id: orderId, status }: EditOrderInput,
+  ): Promise<EditOrderOutput> {
+    try {
+      const order = await this.orders.findOne(orderId, {
+        relations: ['restaurant'],
+      });
+      if (!order) return { ok: false, error: 'Order not found' };
+      if (!this.canSeeOrder(user, order))
+        return { ok: false, error: 'You cannot see that' };
+
+      let canEdit = true;
+      if (user.role === UserRole.Client) canEdit = false;
+      if (user.role === UserRole.Owner) {
+        if (status !== OrderStatus.Cooking && status !== OrderStatus.Cooked)
+          canEdit = false;
+      }
+      if (user.role === UserRole.Delivery) {
+        if (status !== OrderStatus.PickedUp && status !== OrderStatus.Delivered)
+          canEdit = false;
+      }
+      if (!canEdit) return { ok: false, error: 'You cannot do that' };
+
+      await this.orders.save([
+        {
+          id: orderId,
+          status,
+        },
+      ]);
+      return { ok: true };
+    } catch {
+      return { ok: false, error: 'Could not edit order' };
     }
   }
 }
